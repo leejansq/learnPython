8f9899fe88d7b8b8ffb00ebfc2dd2a9f0ef8499b
Silver Bullet <yanjinbin@gmail.com>
2017-04-09 14:31:27 +0800

    从终端写入文件,再从文件读取内容

---
+++ chapter_3/readTextFile.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env Python
+
+'readTextFile.py -- read and display text file'
+
+# get filename
+fname = input('Enter filename: ')
+print
+
+# attempt to open file for reading
+try:
+    fobj = open(fname, 'r')
+except IOError as error:
+    print(error, "### open file error")
+else:  # display contents to the screen
+    for eachLine in fobj:
+        print(eachLine)
+    fobj.close()
