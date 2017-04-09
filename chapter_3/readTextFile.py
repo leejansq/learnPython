3726f6ece6894ff9c1c471227f6d2fcea186ce47
Silver Bullet <yanjinbin@gmail.com>
2017-04-09 14:34:27 +0800

    try-except-else

--- chapter_3/readTextFile.py
+++ chapter_3/readTextFile.py
@@ -7,11 +7,12 @@ fname = input('Enter filename: ')
 print
 
 # attempt to open file for reading
+# try-except-else
 try:
     fobj = open(fname, 'r')
 except IOError as error:
     print(error, "### open file error")
-else:  # display contents to the screen
+else:  # display contents to the screen  # else 语句在没有 except 的时候 执行
     for eachLine in fobj:
         print(eachLine)
     fobj.close()
