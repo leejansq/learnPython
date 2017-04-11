43114a586f41f67fbd04489a8dc6d1d6043170fa
Silver Bullet <yanjinbin@gmail.com>
2017-04-11 17:37:22 +0800

    chapter_8 game over

--- chapter_8/chapter_8.py
+++ chapter_8/chapter_8.py
@@ -96,4 +96,92 @@ for i in range(7):
 else:
     print("for else 来了")
 
-# 迭代器
+# 迭代器 类似 java 的自定义比较器 或者 实现 Iterrator 接口的 数据类型
+# PEP234
+
+# reversed() enumerate() 返回一个迭代器
+
+ivy_schools = ("ZJU", 'ZJOU', "Ucla", "Pku", "Yale", "Columbia", 'Harvard', "Dartmouth")
+iterator = iter(ivy_schools)
+for item in iterator:
+    print(item, end="\t")
+
+    # Python  供给我们的另一个循环机制就是 for 语句.
+    # 它 供了 Python 中最强大的循环结构. 它可以遍历序列成员,
+    # 可以用在 列表解析 和 生成器表达式中, 它会自动地调用迭代器的 next() 方法,
+    #  捕获 StopIteration 异常并结束循环(所有这一切都是在内部发生的).
+#
+# classroom_names = open("classroom.txt")
+# for each_line in classroom_names:
+#     print(each_line)
+# classroom_names.close()
+print()
+top_rank = {1: "ZJOU", 2: "ZJU", 3: "UCLA"}
+
+for key in top_rank:
+    s = Template("rank ${rank} is ${u}")
+    print(s.safe_substitute(rank=key, u=top_rank[key]))
+    # top_rank.popitem()  # 类似 java 的快速失败
+
+# 创建迭代器 在 chapter_13
+
+
+# 列表解析 list comprehension  PEP204
+# map() filter() lambda
+
+var_map = map(lambda x: x ** 2, range(6))
+for item in var_map:
+    print(item, end="\t")
+
+seq = [11, 10, 9, 9, 10, 10, 9, 8, 23, 9, 7, 18, 12, 11, 12]
+print("\n\n------filtered map -------\n")
+filtered_map = filter(lambda x: x % 2 == 0, seq)
+
+for item in filtered_map:
+    print(item, end="\t")
+
+print("\n\n--list comprehension--\n")
+var_map_0 = [x ** 2 for x in range(6) if x % 2 == 0]
+print()
+for item in var_map_0:
+    print(item, end="\t")
+# 生成矩阵
+print()
+matrix = [(x + 1, y + 1) for x in range(4) for y in range(7)]
+print(len(matrix))
+print(repr(matrix))
+
+# 生成器表达式
+# 列表解析:
+# [expr for iter_var in iterable if cond_expr]
+# 生成器表达式:
+# (expr for iter_var in iterable if cond_expr)
+
+# lazy evaluation
+rows = [1, 2, 3, 17]
+
+
+def cols():
+    yield 56
+    yield 2
+    yield 1
+
+
+# 生成器 PEP 289
+print(type(cols()))
+
+x_product_pairs = ((i, j)
+                   for i in rows
+                   for j in cols())
+
+for pair in x_product_pairs:
+    print(pair)
+
+
+def longest():
+    f = open("demo.txt", 'r')
+    longest = max(len(x.strip()) for x in f)
+    f.close()
+    return longest
+
+# 相关模块在 itertools
