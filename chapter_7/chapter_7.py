b368f170d592f9a4b06c4af33538dbe3090d0c46
Silver Bullet <yanjinbin@gmail.com>
2017-04-11 13:10:23 +0800

    chapter_7 dict or hashMap start

--- chapter_7/chapter_7.py
+++ chapter_7/chapter_7.py
@@ -1,1 +1,9 @@
 """映射 集合 ... 高级数据结构类型"""
+
+val_dict = {1: 'a', 2: 'b', 3: 'c'}
+print(val_dict)
+factory_dict = dict((['x', 1], ['y', 2]))
+print(factory_dict)
+
+ddcit = {}.fromkeys(('x', 'y', 'z'), -24)
+print(ddcit)
