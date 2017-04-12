b5f608e94a62b21a7c0baccf5021610fa83b160d
Silver Bullet <yanjinbin@gmail.com>
2017-04-12 18:37:33 +0800

    玩转 import from 语句 名称空间 和 变量2-3个作用域

--- chapter_12/chapter_12.py
+++ chapter_12/chapter_12.py
@@ -5,3 +5,21 @@
 模块 组织
 
 """
+from string import Template as s
+
+# 修改 模块 或者模块属性名称 不过不提倡
+# s = Template
+# del Template
+
+content = s("哈哈---\t\t  ${show}")
+content = content.safe_substitute(show=11)
+print(content)
+
+
+def foo():
+    pass
+
+
+# 无限制的名称空间
+foo.__doc__ = 'Oops, forgot to add doc str above!'
+foo.version = 0.2
