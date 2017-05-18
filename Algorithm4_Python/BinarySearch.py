50cf85f7c0ee148ccb2fd380ec190f30b979e170
yanjinbin <yanjinbin@gmail.com>
2017-05-19 01:16:59 +0800

    binary search

---
+++ Algorithm4_Python/BinarySearch.py
@@ -0,0 +1,22 @@
+def binary_search(key, arrays):
+    length = len(arrays)
+    low = 0
+    high = length - 1
+    while low < high:
+        cursor = int(low + (high - low) / 2)
+        if key < arrays[cursor]:
+            high = cursor - 1
+        elif key > arrays[cursor]:
+            low = cursor + 1
+        return cursor
+
+    return -1
+
+
+key = 11
+
+keys = [1, 3, 4, 8, 9, 11, 32, 34, 46, 76]
+
+print(binary_search(key=11,arrays= keys))
+
+
---
+++ Algorithm4_Python/__init__.py
@@ -0,0 +1,5 @@
+"""
+Python Version 4 Algorithm4
+@author 颜巾斌
+@since 2017-05-18 17:09:50
+"""
